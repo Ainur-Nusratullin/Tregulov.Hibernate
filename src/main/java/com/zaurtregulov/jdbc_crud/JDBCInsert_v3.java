b5299ec06f5f6d925c91fb429b8bc9a16bf7691a
Ainur Nusratullin <dr.nusratullin@yandex.ru>
2025-01-06 22:48:32 +0500

    изменения

--- src/main/java/com/zaurtregulov/jdbc_crud/JDBCInsert_v3.java
+++ src/main/java/com/zaurtregulov/jdbc_crud/JDBCInsert_v3.java
@@ -44,7 +44,7 @@ public class JDBCInsert_v3 {
 
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("commit12345234534563456");
+            System.out.println("Изменения");
         } finally {
             try {
                 connection.close();
